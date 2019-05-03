e68585c72f218ea0801dee11a5e12c4bfd844726
JVicente7037 <38221132+JVicente7037@users.noreply.github.com>
2019-05-03 13:50:46 -0500

    Add files via upload

---
+++ Singleton.java
@@ -0,0 +1,37 @@
+package servlet;
+
+import java.util.ArrayList;
+
+public class Singleton {
+	private static Singleton instanciaUnica;
+	 
+    private Singleton() {}
+    ArrayList<Usuario> reservas = new ArrayList<Usuario>();
+    Usuario u;
+    public static Singleton getInstance() {
+        if (instanciaUnica == null) {
+            instanciaUnica = new Singleton();
+        }
+     
+        return instanciaUnica;
+    }
+    
+    public  boolean ingresar(String login, String pass) {
+		if(u.ingresar(login, pass)== true) {
+			return true;
+			
+		}
+		else {
+    	return false;
+		}
+    }
+		
+	public void setRegistro(String correo,String password,String tipo) {
+		u.setLogin(correo);
+		u.setPassword(password);
+		u.SetTipoUsuario(tipo);
+    	
+    }
+    
+    
+}
