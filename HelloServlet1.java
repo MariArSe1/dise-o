9b61e6d9f3c8cf4b751ed726fae11bb14a1e1a0d
MariArSe1 <49568817+MariArSe1@users.noreply.github.com>
2019-05-10 13:18:00 -0500

    Update HelloServlet1.java

--- HelloServlet1.java
+++ HelloServlet1.java
@@ -10,16 +10,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
-        name = "MyServlet", 
-        urlPatterns = {"/hello1"}
+         name = "ingresar", 
+        urlPatterns = {"/ingresar"}
+        
     )
 public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        String ingresacorreo = request.getParameter("ingresacorreo");
-        String contrasena = request.getParameter("contrasena");
+        Singleton tempo = Singleton.laconstructora();
+
+        String search = req.getParameter("ingresacorreo");
+        Comprador project = tempo.ingresar(search);
+        String data = project.toString();
+        ServletOutputStream out = resp.getOutputStream();
+		        out.write(data.getBytes());
+		        out.flush();
+		        out.close();
+
 
         User user = userService.find(ingresacorreo, contrasena);
     
